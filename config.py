fe3ed667885dbe09b80b8166b327467a407fe22e
Katherine0811 <107659423+Katherine0811@users.noreply.github.com>
2022-09-02 00:15:29 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cauzymcpidv8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leeyanjie-employee"
+customregion = "us-east-1"
